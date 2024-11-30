baa7af920013cd1c8f8ae787e7034b91716bfee7
swethats2001 <121440689+swethats2001@users.noreply.github.com>
2024-11-30 15:51:29 +0530

    Add files via upload

---
+++ 1_PythonFundamentals/1_Python Fundamentals.py
@@ -0,0 +1,63 @@
+#1. Write Python code that prints your name, student number and email address.
+# name=input("ENTER YOUR NAME:")
+# stu_num=input("ENTER YOUR STUDENT NUMBER:")
+# email=input("ENTER YOUR EMAIL:")
+# print("NAME: ",name)
+# print("STUDENT NUMBER: ",stu_num)
+# print("EMAIL: ",email)
+
+#2. Write Python code that prints your name, student number and email address using escape sequences.
+# name=input("ENTER YOUR NAME:")
+# stu_num=input("ENTER YOUR STUDENT NUMBER:")
+# email=input("ENTER YOUR EMAIL:")
+# print("Name: ",name,"\nStudent Number: ",stu_num,"\nEmail: ",email)
+
+#3. Write Python code that add, subtract, multiply and divide the two numbers. You can use the two numbers 14 and 7
+# num1=14
+# num2=7
+# print("Addition(14+7): ",num1+num2)
+# print("Product(14*7): ",num1*num2)
+# print("Subtraction(14-7): ",num1-num2)
+# print("Division(14/7): ",num1/num2)
+
+#4. Write Python code that displays the numbers from 1 to 5 as steps.
+# print("1","\n2","\n3","\n4","\n5")
+
+#5. Write Python code that outputs the following sentence (including the quotation marks and line break) to the screen:
+# "SDK" stands for "Software Development Kit", whereas "IDE" stands for "Integrated Development Environment"
+# print("\"SDK\" stands for \"Software Development Kit\", whereas \"IDE\" stands for \"Integrated Development Environment\"")
+
+#6.  6 Practice and check the output
+# print("python is an \"awesome\" language.")
+# print("python\n\t2023")
+# print('I\'m from Entri.\b')
+# print("\65")
+# print("\x65")
+# print("Entri", "2023", sep="\n")
+# print("Entri", "2023", sep="\b")
+# print("Entri", "2023", sep="*", end="\b\b\b\b")
+
+#7. Define the variables below. Print the types of each variable. What is the sum of your variables?
+# (Hint: use a type conversion function.) What datatype is the sum?
+# num = 23
+# textnum = "57"
+# decimal = 98.3
+# print(type(num),type(textnum),type(decimal))
+# textnum2=int(textnum)
+# sum=num+textnum2+decimal
+# print("23+57+98.3 = ",sum)
+
+#8. calculate the number of minutes in a year using variables for each unit of time.
+# print a statement that describes what your code does also.
+# nod=365
+# hours=24
+# minutes=60
+# min_in_year=nod*hours*minutes
+# print("Number of minutes in an an year: ",min_in_year)
+# print("This code takes three variables to store \"no of days in an year\", \"minute in a hour\", \"hours in a day\", "
+#       "\nthen calculates the total minutes in an year by multiplying these 3 variables")
+
+#9. Write Python code that asks the user to enter his/her name and then output/prints his/her name with a greeting.
+# name=input("Please enter your name: ")
+# print("Hi {}, welcome to Python programming :)".format(name))
+
