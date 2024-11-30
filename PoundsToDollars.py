0416bc0a503fefb41089d7f49295022ea8f99818
swethats2001 <121440689+swethats2001@users.noreply.github.com>
2024-11-30 15:52:12 +0530

    Delete PoundsToDollars.py

--- PoundsToDollars.py
+++
@@ -1,4 +0,0 @@
-# Write a program that asks the user to enter an amount in pounds (£) and the program calculates and converts an amount in dollar ($)
-pounds=int(input("Enter an amount in pounds (£): "))
-dollar=1.36 * pounds
-print("Amount in Dollars($): ",round(dollar,2))
