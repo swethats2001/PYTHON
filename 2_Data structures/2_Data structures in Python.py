f96dec94b1c18607e10f01dfea40713fd0dcc2f1
swethats2001 <121440689+swethats2001@users.noreply.github.com>
2024-12-07 19:08:00 +0530

    Add files via upload

---
+++ 2_Data structures/2_Data structures in Python.py
@@ -0,0 +1,48 @@
+# Topic :List
+# Exercise
+# Q1. Create a list of 5 random numbers and print the list.
+# l1=[1,5,98,50,23]
+# print("The list:",l1)
+
+# Q2. Insert 3 new values to the list and print the updated list.
+# l1.insert(3,120)
+# l1.insert(4,600)
+# l1.insert(5,100)
+# print("The list:",l1)
+
+# Q3. Try to use a for loop to print each element in the list.
+# for i in l1:
+#     print(i)
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# Topic: Dictionary
+# Exercise
+# Q1. Create a dictionary with keys 'name', 'age', and 'address' and values 'John', 25, and 'New York' respectively.
+# person={"name":"John","age":"25","address":"New York"}
+# print(person)
+# print(type(person))
+# Q2. Add a new key-value pair to the dictionary created in Q1 with key 'phone' and value '1234567890'.
+# person["phone"]="1234567890"
+# print(person)
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# Topic: Set
+# Exercise
+# Q1.Create a set with values 1, 2, 3, 4, and 5.
+# numbers={1,2,3,4,5}
+# print(numbers,type(numbers))
+# Q2. Add the value 6 to the set created in Q1.
+# numbers.add(6)
+# print(numbers)
+# Q3. Remove the value 3 from the set created in Q1.
+# numbers.remove(3)
+# print(numbers)
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# Topic:Tuple
+# Exercise
+# Q1. Create a tuple with values 1, 2, 3, and 4
+# tupnumbers=(1,2,3,4)
+# print(tupnumbers,type(tupnumbers))
+# Q2. Print the length of the tuple created in Q1.
+# print(len(tupnumbers))
